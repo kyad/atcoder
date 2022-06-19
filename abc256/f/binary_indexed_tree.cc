c2a14e2eaaeb9e0cb76680e1fccdda944cf1c71f
kyad <53517611+kyad@users.noreply.github.com>
2022-06-19 17:00:12 +0900

    Add ABC256 F

---
+++ abc256/f/binary_indexed_tree.cc
@@ -0,0 +1,115 @@
+// https://atcoder.jp/contests/abc256/tasks/abc256_f
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+#include <atcoder/modint>
+using namespace atcoder;
+using mint = modint998244353;
+
+template<class T> struct binary_indexed_tree {
+  int N;
+  vector<T> bit;  // 1-indexed
+  binary_indexed_tree() {}
+  binary_indexed_tree(int n) : N(n) {
+    bit.resize(N + 1, 0);
+  }
+  void resize(int n) {
+    N = n;
+    bit.resize(N + 1, 0);
+  }
+  // Add x to a[k], O(logN)
+  // k: 1-indexed
+  void add1(int k, T x) {
+    for (; k <= N; k += (k & -k)) {
+      bit[k] += x;
+    }
+  }
+  // Add x to a[k], O(logN)
+  // k: 0-indexed
+  void add0(int k, T x) {
+    add1(k + 1, x);
+  }
+  // Returns a[1] + a[2] + ... + a[k], O(logN)
+  // k: 1-indexed
+  T sum1(int k) {
+    T ret = 0;
+    for (; k > 0; k -= (k & -k)) {
+      ret += bit[k];
+    }
+    return ret;
+  }
+  // Returns a[0] + a[1] + ... + a[k], O(logN)
+  // k: 0-indexed
+  T sum0(int k) {
+    return sum1(k + 1);
+  }
+  // Returns minimum x such that a[1] + a[2] + ... + a[x] >= w, O(logN)
+  // return: 1-indexed (0: not found)
+  T lower_bound1(T w) {
+    if (w <= 0) {
+      return 0;
+    } else {
+      T x = 0;
+      int r = 1;
+      while (r < N) {
+        r = r << 1;
+      }
+      for (int len = r; len > 0; len = len >> 1) {
+        if (x + len < N && bit[x + len] < w) {
+          w -= bit[x + len];
+          x += len;
+        }
+      }
+      return x + 1;
+    }
+  }
+  // return: 0-indexed (-1: not found)
+  T lower_bound0(T w) {
+    return lower_bound1(w) - 1;
+  }
+};
+
+int main() {
+  int N, Q;
+  cin >> N >> Q;
+  vector<int> A(N);
+  for (int n = 0; n < N; n++) {
+    cin >> A.at(n);
+  }
+  // A[i], i*A[i], i^2*A[i]を要素に持つbinary indexed tree
+  vector<binary_indexed_tree<mint> > bit(3, binary_indexed_tree<mint>(N));
+  // 1点更新
+  auto bit_add = [&](int i, mint a) {
+    for (int k = 0; k < 3; k++) {
+      bit[k].add0(i, a);
+      a *= i;
+    }
+  };
+  // 初期化
+  for (int n = 0; n < N; n++) {
+    bit_add(n, A.at(n));
+  }
+  for (int q = 0; q < Q; q++) {
+    int type;
+    cin >> type;
+    if (type == 1) {
+      int x, v;
+      cin >> x >> v;
+      x--;
+      bit_add(x, v - A.at(x));
+      A.at(x) = v;
+    } else {
+      int x;
+      cin >> x;
+      x--;
+      mint ans = bit[2].sum0(x);
+      ans += bit[1].sum0(x) * (-2 * x - 3);
+      ans += bit[0].sum0(x) * (x + 1) * (x + 2);
+      ans /= 2;
+      cout << ans.val() << endl;
+    }
+  }
+  return 0;
+}
