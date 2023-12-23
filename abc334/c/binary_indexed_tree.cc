ae245927fafdc578de3e1864f411bd64c63ef6c7
kyad <53517611+kyad@users.noreply.github.com>
2023-12-23 23:24:27 +0900

    Update ABC334 C

---
+++ abc334/c/binary_indexed_tree.cc
@@ -0,0 +1,174 @@
+#include <algorithm>
+#include <cassert>
+#include <iostream>
+#include <set>
+#include <vector>
+using namespace std;
+
+template<class T> struct binary_indexed_tree {
+  int N;
+  vector<T> bit;  // 1-indexed
+  binary_indexed_tree() {}
+  binary_indexed_tree(int n) : N(n) { bit.resize(N + 1, 0); }
+  void resize(int n) {
+    N = n;
+    bit.resize(N + 1, 0);
+  }
+  // Add x to a[k], 1-indexed, O(logN)
+  void add1(int k, T x) {
+    for (; k <= N; k += (k & -k)) {
+      bit[k] += x;
+    }
+  }
+  // Add x to a[k], 0-indexed, O(logN)
+  void add0(int k, T x) { add1(k + 1, x); }
+  // Returns sum for [0, k], 1-indexed, O(logN)
+  T sum1_closed(int k) const {
+    T ret = 0;
+    for (; k > 0; k -= (k & -k)) {
+      ret += bit[k];
+    }
+    return ret;
+  }
+  // Returns sum for [l, r], 1-indexed, O(logN)
+  T sum1_closed(int l, int r) const {
+    if (l == 1) {
+      return sum1_closed(r);
+    } else {
+      return sum1_closed(r) - sum1_closed(l - 1);
+    }
+  }
+  // Returns sum for [l, r), 1-indexed, O(logN)
+  T sum1_open(int l, int r) const {
+    if (l == r) {
+      return 0;
+    } else {
+      return sum1_closed(l, r - 1);
+    }
+  }
+  // Returns sum for [0, k], 0-indexed, O(logN)
+  T sum0_closed(int k) const { return sum1_closed(k + 1); }
+  // Returns sum for [0, k), 0-indexed, O(logN)
+  // 半開区間の場合は[0, k)が空集合(k==0)の場合にも使え、0を返す。
+  T sum0_open(int k) const {
+    if (k == 0) {
+      return 0;
+    }
+    return sum1_closed(k);
+  }
+  // Returns sum for [l, r], 0-indexed, O(logN)
+  T sum0_closed(int l, int r) const { return sum1_closed(l + 1, r + 1); }
+  // Returns sum for [l, r), 0-indexed, O(logN)
+  // 半開区間の場合は[l, r)が空集合(l==r)の場合にも使え、0を返す。
+  T sum0_open(int l, int r) const {
+    if (l == r) {
+      return 0;
+    }
+    return sum1_closed(l + 1, r + 1 - 1);
+  }
+  // Returns a[k]
+  T get1(int k) const { return (k == 1) ? sum1_closed(k) : (sum1_closed(k) - sum1_closed(k - 1)); }
+  T get0(int k) const { return get1(k + 1); }
+  // Update, 1-indexed, O(logN)
+  void update1(int k, T x) {
+    T old = get1(k);
+    add1(k, -old);
+    add1(k, x);
+  }
+  // Update, 0-indexed, O(logN)
+  void update0(int k, T x) { add1(k + 1, x); }
+  // Returns minimum x such that a[1] + a[2] + ... + a[x] >= w, O(logN)
+  // return: 1-indexed (0: not found)
+  T lower_bound1(T w) {
+    if (w <= 0) {
+      return 0;
+    } else {
+      T x = 0;
+      int r = 1;
+      while (r < N) {
+        r = r << 1;
+      }
+      for (int len = r; len > 0; len = len >> 1) {
+        if (x + len < N && bit[x + len] < w) {
+          w -= bit[x + len];
+          x += len;
+        }
+      }
+      return x + 1;
+    }
+  }
+  // return: 0-indexed (-1: not found)
+  T lower_bound0(T w) { return lower_bound1(w) - 1; }
+  // atcoder::fenwick_treeのインタフェースに合わせる
+  void add(int p, T x) { add0(p, x); }
+  T sum(int l, int r) { return sum0_open(l, r); }
+};
+
+template<class X> ostream& operator<<(ostream& os, const binary_indexed_tree<X> &bit) {
+  int N = bit.N;
+  vector<X> v(N);
+  for (int n = 0; n < N; n++) { v.at(n) = bit.get0(n); }
+  os << '[';
+  for (int i = 0; i < N; i++) {
+    //os << '['<< i << ']=';
+    os << v[i];
+    const int MOD = -1;
+    if (i == N - 1) { continue; }
+    else if (MOD > 0 && i % MOD == (MOD - 1)) { os << '\n'; }
+    else { os << ' '; }
+  }
+  os << ']';
+  return os;
+}
+
+// BITの区間加算・1点取得はimos法の値をbitで管理する。
+// binary_indexed_tree<long long> bit(N + 1);
+// [l, r]にxを加算：add(l, x), add(r + 1, x)
+// 1点iの取得：sum(0, i)
+// cf. https://atcoder.jp/contests/abc253/tasks/abc253_f
+
+// A[i]にiの多項式を掛けた値の和を計算したい場合は、
+// iについて展開して、i^k*A[i]を要素に持つbinary indexed treeを考える
+
+// Example
+// binary_indexed_tree<int> bit(N);
+// bit.add(i, x);
+// int sum = bit.sum(l, r);
+
+
+int main() {
+  int N, K;
+  cin >> N >> K;
+  vector<int> B(N, 0);
+  for (int k = 0; k < K; k++) {
+    int a;
+    cin >> a;
+    a--;
+    B.at(a) = 1;
+  }
+  for (int i = 0; i < N - 1; i++) {
+    B.at(i + 1) += B.at(i);
+    B.at(i + 1) %= 2;
+  }
+
+  binary_indexed_tree<int> bit(N);
+  for (int i = 0; i < N; i++) {
+    if (B.at(i) == 1) {
+      bit.update0(i, 1);
+    }
+  }
+
+  const int INF = 1e9;
+  long long ans = INF;
+  int b = 2 * N - K;
+  if (b % 2 == 1) { // 奇数
+    for (int i = 0; i < N; i++) {
+      long long now = bit.sum(0, i) + (N - i) - bit.sum(i, N);
+      ans = min(ans, now);
+    }
+  } else {  // 偶数
+    ans = bit.sum(0, N);
+  }
+  cout << ans << endl;
+  return 0;
+}
